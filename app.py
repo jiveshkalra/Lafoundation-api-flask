a6be5d2fed0480281f31fd76c68e80b9cfcbf44c
jiveshkalra <127482713+jiveshkalra@users.noreply.github.com>
2023-07-26 09:36:46 +0530

    init

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import pandas as pd
 import pickle
 
@@ -7,9 +7,7 @@ app = Flask(__name__)
 # Load the model from the pickle file
 with open('model.pkl', 'rb') as file:
     model = pickle.load(file)
-
-@app.route('/')
-def predict_crop():
+def get_data():
     # Get the input parameters from the GET request
     temperature = float(request.args.get('temperature', default=0.0))
     humidity = float(request.args.get('humidity', default=0.0))
@@ -23,13 +21,29 @@ def predict_crop():
         'ph': [ph],
         'rainfall': [rainfall]
     })
+    return new_data , temperature , humidity,ph,rainfall
 
+@app.route('/api')
+def predict_crop_api():
+    new_data, temperature , humidity,ph,rainfall = get_data()
     try:
         # Make predictions for new data using the loaded model
         new_predictions = model.predict(new_data)
 
         # Return the predictions as a JSON response
         return jsonify({'predictions': new_predictions.tolist()})
+    except Exception as e:
+        # Return an error message if something goes wrong
+        return jsonify({'error': str(e)})
+
+@app.route('/')
+def predict_crop():
+    new_data, temperature , humidity,ph,rainfall = get_data()
+    try:
+        # Make predictions for new data using the loaded model
+        new_predictions = model.predict(new_data)
+        # Return the predictions as a JSON response
+        return render_template('index.html', temperature=temperature, humidity=humidity, ph=ph, rainfall=rainfall, predictions=new_predictions)
     except Exception as e:
         # Return an error message if something goes wrong
         return jsonify({'error': str(e)})
