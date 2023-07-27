64cfc2a66c17a56a667c9e49258c335582f9ee9b
BooyahSquadians69 <jiveshkalra69@gmail.com>
2023-07-27 21:20:33 +0530

    test

--- flaskapp.py
+++ flaskapp.py
@@ -1,34 +1,29 @@
 from flask import Flask, request, jsonify, render_template
 import pandas as pd
-import pickle
+# import pickle
+import joblib
 
 app = Flask(__name__)
 
 # Load the model from the pickle file
-with open('model.pkl', 'rb') as file:
-    model = pickle.load(file)
+# with open('model.pkl', 'rb') as file:
+#     model = pickle.load(file)
+model = joblib.load('final_model.joblib')
+
 def get_data():
     # Get the input parameters from the GET request
     temperature = float(request.args.get('temperature', default=0.0))
     humidity = float(request.args.get('humidity', default=0.0))
     ph = float(request.args.get('ph', default=0.0))
     rainfall = float(request.args.get('rainfall', default=0.0))
-
-    # Create a DataFrame with the input data
-    new_data = pd.DataFrame({
-        'temperature': [temperature],
-        'humidity': [humidity],
-        'ph': [ph],
-        'rainfall': [rainfall]
-    })
-    return new_data , temperature , humidity,ph,rainfall
+    return temperature, humidity, ph, rainfall
 
 @app.route('/api')
 def predict_crop_api():
-    new_data, temperature , humidity,ph,rainfall = get_data()
+    temperature, humidity, ph, rainfall = get_data()
     try:
         # Make predictions for new data using the loaded model
-        new_predictions = model.predict(new_data)
+        new_predictions = model.predict([[temperature, humidity, ph, rainfall]])
 
         # Return the predictions as a JSON response
         return jsonify({'predictions': new_predictions.tolist()})
@@ -38,10 +33,11 @@ def predict_crop_api():
 
 @app.route('/')
 def predict_crop():
-    new_data, temperature , humidity,ph,rainfall = get_data()
+    temperature, humidity, ph, rainfall = get_data()
     try:
         # Make predictions for new data using the loaded model
-        new_predictions = model.predict(new_data)
+        new_predictions = model.predict([[temperature, humidity, ph, rainfall]])
+
         # Return the predictions as a JSON response
         return render_template('index.html', temperature=temperature, humidity=humidity, ph=ph, rainfall=rainfall, predictions=new_predictions)
     except Exception as e:
